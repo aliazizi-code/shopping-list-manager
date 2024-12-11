ab5c4c2ff4cc25531e43824435759f0c299847cc
aliazizi-code <devaliazizi@gmail.com>
2024-12-11 17:45:44 +0330

    Add JWT token refresh URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts import views
 
 urlpatterns = [
     path('request/', views.OTPRequestView.as_view(), name='request'),
-    path('verify/', views.OTPVerifyView.as_view(), name='verify'),
+    path('verify/', views.OTPVerifyView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
