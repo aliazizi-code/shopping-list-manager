ad3be6b47414af387b57ee635ef734198f6e46e1
aliazizi-code <devaliazizi@gmail.com>
2024-12-11 17:40:57 +0330

    Complete send_otp function to send OTP via email

--- utils/send_otp.py
+++ utils/send_otp.py
@@ -1,7 +1,20 @@
 from accounts.models import OTPRequest
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 
-def send_otp(data):
-    otp = OTPRequest.objects.filter(email=data['email']).first()
+def send_otp(email):
+    otp = OTPRequest.objects.filter(email=email).first()
     if otp:
-        print(f"Code: {otp.password}")
+        email = EmailMessage(
+            subject='Code',
+            body=otp.password,
+            to=email
+        )
+
+        try:
+            email.send()  # Send the email
+            print("Email sent successfully!")
+        except Exception as e:
+            print(f"Error sending email: {e}")
+            print(f"Code: {otp.password}")
