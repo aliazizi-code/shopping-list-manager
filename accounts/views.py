fbb21b6dec20be26b7b2be4f70f4f90fe6a46d64
aliazizi-code <devaliazizi@gmail.com>
2024-12-14 13:09:00 +0330

    Implement @extend_schema in OTPRequestView and OTPVerifyView for improved API documentation

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -12,21 +13,52 @@ from utils.send_otp import send_otp
 class OTPRequestView(APIView):
     serializer_class = serializers.OTPRequestSerializer
 
+    @extend_schema(
+        request=serializers.OTPRequestSerializer,
+        responses={
+            200: serializers.OTPResponseSerializer,
+            201: serializers.OTPResponseSerializer,
+            400: "Invalid Input",
+        },
+        description="Request an OTP for the provided email."
+    )
+
     def post(self, request: Request):
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             data = serializer.validated_data
             otp_request, created = OTPRequest.objects.get_or_create(email=data['email'])
+
             otp_request.refresh(data) if not created else None
             send_otp(data['email'])
-            return Response(status=status.HTTP_200_OK if not created else status.HTTP_201_CREATED)
+
+            response_data = {
+                "message": "OTP sent successfully",
+                "email": data['email']
+            }
+            response_serializer = serializers.OTPResponseSerializer(data=response_data)
+            response_serializer.is_valid(raise_exception=True)
+
+            return Response(
+                response_serializer.data,
+                status=status.HTTP_200_OK if not created else status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class OTPVerifyView(APIView):
     serializer_class = serializers.VerifyOTPRequestSerializer
+
+    @extend_schema(
+        request=serializers.VerifyOTPRequestSerializer,
+        responses={
+            200: 'Successful login with access and refresh tokens',
+            401: 'Unauthorized - Invalid OTP',
+            400: 'Invalid Input',
+        },
+        description="Verify the OTP for the provided email."
+    )
 
     def post(self, request: Request):
         serializer = self.serializer_class(data=request.data)
