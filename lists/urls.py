5b6d2f6a593adf74ed9dc44d57b47fc8883ab1e4
aliazizi-code <devaliazizi@gmail.com>
2024-12-14 13:39:08 +0330

    Refactor URL patterns and endpoint names for clarity and consistency

--- lists/urls.py
+++ lists/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('', views.ListViewSet.as_view({'get': 'list', 'post': 'create'}), name='lists'),
+    path('lists/', views.ListViewSet.as_view({'get': 'list', 'post': 'create'}), name='list_create'),
     path('search/', views.SearchView.as_view(), name='search'),
-    path('item/<slug:slug>/', views.ItemViewSet.as_view({'patch': 'partial_update', 'delete': 'destroy'}), name='item_detail'),
-    path('<slug:slug>/', views.ListViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}), name='list_detail'),
-    path('<slug:slug>/items/', views.ItemViewSet.as_view({'post': 'create'}), name='items'),
+    path('item/<slug:slug>/', views.ItemViewSet.as_view({'patch': 'partial_update', 'delete': 'destroy'}),
+         name='item_detail'),
+    path('list/<slug:slug>/',
+         views.ListViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}),
+         name='list_detail'),
+    path('list/<slug:slug>/items/', views.ItemViewSet.as_view({'post': 'create'}), name='items'),
 ]
